577d3e7e493cbde39faa156e1f843b3ecce45061
GeometricEath <geo.fsv@gmail.com>
2023-08-21 13:12:59 +0300

    feat: Добавлены конструкторы ошибок http

---
+++ utils/httpErrors/index.js
@@ -0,0 +1,15 @@
+const AuthError = require('./AuthError');
+const BadRequest = require('./BadRequest');
+const DuplicateKeyError = require('./DuplicateKeyError');
+const Forbidden = require('./Forbidden');
+const NotFound = require('./NotFound');
+const ValidationError = require('./ValidationError');
+
+module.exports = {
+  AuthError,
+  BadRequest,
+  DuplicateKeyError,
+  Forbidden,
+  NotFound,
+  ValidationError,
+};
