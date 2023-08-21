8efc6d176a731afd215aa09795721056aab4cfde
GeometricEath <geo.fsv@gmail.com>
2023-08-21 11:36:54 +0300

    feat: Добавлена валидация email

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -11,6 +12,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    validate: validator.isEmail,
   },
   password: {
     type: String,
