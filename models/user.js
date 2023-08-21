003c878e35effe972c2ef0b2bf6c652394bfa4bf
GeometricEath <geo.fsv@gmail.com>
2023-08-21 11:36:29 +0300

    feat: Добавлены схемы user и movie

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.methods.toJSON = () => {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+module.exports = mongoose.model('user', userSchema);
