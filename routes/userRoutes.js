c797e28ffaa91802c76549bfed9413af05d53193
GeometricEath <geo.fsv@gmail.com>
2023-08-22 11:07:51 +0300

    feat: Добавлена валидация роута PATCH users/me

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { getUser, updateUser } = require('../controllers/userController');
+const updateUserValidator = require('../utils/userSchemaValidatior');
 
 router.get('/me', getUser);
-router.patch('/me', updateUser);
+router.patch('/me', updateUserValidator, updateUser);
 
 module.exports = router;
